73b28a0e1a0abbff820a34a8b066d9dbb4bbeb86
Andy Li <liandy1028@gmail.com>
2022-04-24 01:02:36 -0700

    bars

--- CCGJ2022/Assets/Resources/Scripts/TooltipRenderer/TooltipRenderer.cs
+++ CCGJ2022/Assets/Resources/Scripts/TooltipRenderer/TooltipRenderer.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class TooltipRenderer : MonoBehaviour
 {
     public bool renderUp;
     private IngredientObject ingredient;
+    public GameObject slider;
+    private List<GameObject> sliders = new List<GameObject>();
 
     public void ShowTooltip() 
     {
@@ -24,6 +27,21 @@ public class TooltipRenderer : MonoBehaviour
         if (renderUp) gameObject.transform.position += new Vector3(0,4,0);
         else gameObject.transform.position += new Vector3(0,-4,0);
         gameObject.GetComponentInChildren<TMPro.TextMeshPro>().text = ingredient.ingredientName;
+
+        foreach (var attribute in ingredient.AttributeDict) 
+        {
+            sliders.Add(GameObject.Instantiate(slider, gameObject.transform));
+            sliders.Last().transform.position += sliders.Count * new Vector3(0, -1, 0);
+            sliders.Last().GetComponentInChildren<TMPro.TextMeshPro>().text = attribute.Key.displayName;
+            var sprite = sliders.Last().GetComponentInChildren<SpriteRenderer>();
+            var scale = sprite.transform.localScale;
+            var pos = sprite.transform.position;
+            pos.x -= (100 - attribute.Value) / 40;
+            scale.x = attribute.Value / 20 + 0.2f;
+            sprite.transform.localScale = scale;
+            sprite.transform.position = pos;
+            sprite.color = attribute.Key.attributeColor;
+        }
     }
 
     // Update is called once per frame
