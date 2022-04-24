3041be1c7edea27011aa500950490686ee0cf4d1
Andy Li <liandy1028@gmail.com>
2022-04-24 10:11:31 -0700

    deleted template code

--- CCGJ2022/Assets/Resources/Scripts/StatsBarMenu/StatsBarMenu.cs
+++ CCGJ2022/Assets/Resources/Scripts/StatsBarMenu/StatsBarMenu.cs
@@ -14,29 +14,6 @@ public class StatsBarMenu : MonoBehaviour
     
     private float sliderSpeed = 0.5f;
     private PotionAttributeCollection attributes;
-    sealed class MyAttribute : System.Attribute
-    {
-        // See the attribute guidelines at
-        //  http://go.microsoft.com/fwlink/?LinkId=85236
-        readonly string positionalString;
-        
-        // This is a positional argument
-        public MyAttribute(string positionalString)
-        {
-            this.positionalString = positionalString;
-            
-            // TODO: Implement code here
-            throw new System.NotImplementedException();
-        }
-        
-        public string PositionalString
-        {
-            get { return positionalString; }
-        }
-        
-        // This is a named argument
-        public int NamedInt { get; set; }
-    }
 
     private struct SliderStruct 
     {
